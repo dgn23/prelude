defa589015510858decb8a6311a434dd7e7a184c
Dustin Neuman <dustin.neuman@gmail.com>
2015-08-27 15:17:59 -0700

    Mini-post-post-commit
    Custom.el and emacs-color-theme-solarized

---
+++ personal/custom.el
@@ -0,0 +1,49 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-dictionary-directories
+   (quote
+    ("/Users/dustinneuman/.emacs.d/elpa/auto-complete-20150618.1949/dict" "/Users/dustinneuman/dicts")))
+ '(ac-user-dictionary (quote (Users/dustinneuman/dicts/LocalDictionary)))
+ '(auth-source-protocols
+   (quote
+    ((imap "imap" "imaps" "143" "993")
+     (pop3 "pop3" "pop" "pop3s" "110" "995")
+     (ssh "ssh" "22")
+     (sftp "sftp" "115")
+     (smtp "smtp" "25" "587"))))
+ '(blink-cursor-blinks 20)
+ '(completion-search-distance 0)
+ '(cursor-type (quote box))
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("3b24f986084001ae46aa29ca791d2bc7f005c5c939646d2b800143526ab4d323" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "a0feb1322de9e26a4d209d1cfa236deaf64662bb604fa513cca6a057ddf0ef64" "a6289444757a2ccc56e3cbfefd9c93d9570788847e1f04c16de81483790a1edf" "804dd83b709ca22fa6286bee885b8175b7e11e6d941c729ab299aefaf5e20041" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(dynamic-completion-mode t)
+ '(el-get-is-lazy t)
+ '(package-selected-packages
+   (quote
+    (company-ghc ebal haskell-emacs haskell-emacs-base haskell-emacs-text haskell-snippets hayoo helm-hoogle hi2 shm aurora-theme base16-theme boron-theme cycle-themes deep-thought-theme fish-mode flatland-black-theme flatland-theme flatui-theme helm-dash helm-mu helm-themes helm-wordnet hipster-theme lavender-theme lenlen-theme leuven-theme load-theme-buffer-local material-theme per-buffer-theme wanderlust with-editor zenburn-theme alect-themes sequences bog bookmark+ browse-kill-ring calfw cdlatex cl-format cl-generic cl-lib color-identifiers-mode color-theme-buffer-local color-theme-cobalt color-theme-emacs-revert-theme color-theme-sanityinc-solarized color-theme-sanityinc-tomorrow color-theme-solarized color-theme-tangotango color-theme-twilight color-theme-vim-insert-mode color-theme-wombat color-theme-x color-theme-zenburn command-t company-auctex company-math company-nixos-options company-statistics concurrent corral counsel dakrone-theme darcula-theme darktooth-theme dash dash-at-point dash-functional evil-surround sly smart-mode-line smart-mode-line-powerline-theme smex spacegray-theme annotate anzu artbollocks-mode auctex-latexmk auctex-lua auto-complete-auctex auto-package-update autumn-light-theme avy-zap ac-ispell ac-math ac-sly ace-flyspell ace-isearch ace-jump-buffer ace-jump-helm-line ace-jump-mode ace-jump-zap ace-window achievements afternoon-theme ag fontawesome helm-swoop auto-complete use-package pallet exec-path-from-shell)))
+ '(paradox-automatically-star nil)
+ '(paradox-github-token "c85391996956a3bbc7fe049a4ea2663f05806153")
+ '(predictive-add-to-dict-ask nil)
+ '(predictive-auto-add-to-dict t)
+ '(predictive-completion-speed 0.5)
+ '(predictive-fuzzy-completion nil)
+ '(predictive-which-dict-mode nil)
+ '(pretty-symbol-categories (lambda relational logical nil))
+ '(yas-snippet-dirs
+   (quote
+    ("/Users/dustinneuman/.emacs.d/elpa/common-lisp-snippets-20150603.944/snippets" "~/.emacs.d/snippets" "/Users/dustinneuman/.emacs.d/elpa/haskell-snippets-20150612.1239/snippets" "/Users/dustinneuman/.emacs.d/elpa/django-snippets-20131229.811/snippets")) nil (yasnippet))
+ '(yas-snippet-revival nil))
+
+;;
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
